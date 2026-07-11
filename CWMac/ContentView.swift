c539e654ff41e647a172c989940b46382acdb32f
mikagosz <mikagosz@noreply.local>
2026-07-11 09:13:21 +0200

    Initial Commit

---
+++ CWMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CWMac
+//
+//  Created by mikagosz on 11/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
