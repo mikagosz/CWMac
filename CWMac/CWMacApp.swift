4d0b309120cc286d8d74ecda1590971bc131a757
mikagosz <support@fractal8.eu>
2026-07-11 11:14:40 +0200

    Nowa wersja CWMac: usypianie/wyłączanie z licznikiem i ikoną w pasku menu
    - Odliczanie (async/await) z wyborem akcji uśpij/wyłącz i presetami czasu
    - Fioletowy motyw i ikona aplikacji w oknie + badge z wersją
    - NSStatusItem: licznik minut z symbolem zzz, kolor/mono, klik = menu, dwuklik = okno
    - Chowanie do paska (accessory podczas odliczania), X nie zamyka programu
    - Ustawienia: widoczność i styl ikony w pasku; przycisk zamykania aplikacji

--- CWMac/CWMacApp.swift
+++ CWMac/CWMacApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import AppKit
+
+/// Utrzymuje aplikację przy życiu po zamknięciu okna i tworzy ikonę w pasku menu.
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusController: StatusItemController?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        UserDefaults.standard.register(defaults: [
+            "menuBarMonochrome": true,
+            "showMenuBarIcon": true
+        ])
+        statusController = StatusItemController(manager: .shared)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        false
+    }
+}
 
 @main
 struct CWMacApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    private let manager = CountdownManager.shared
+
     var body: some Scene {
-        WindowGroup {
+        Window("CWMac", id: "main") {
             ContentView()
+                .environment(manager)
+                .tint(.cwPurple)
+        }
+        .windowResizability(.contentSize)
+
+        Settings {
+            SettingsView()
         }
     }
 }
