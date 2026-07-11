9efc03237d72ad208767eb8da3aedb398d8feb3e
mikagosz <support@fractal8.eu>
2026-07-11 09:13:21 +0200

    Initial Commit

---
+++ CWMac/CWMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWMacApp.swift
+//  CWMac
+//
+//  Created by mikagosz on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CWMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
